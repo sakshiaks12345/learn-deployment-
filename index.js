7edd6fb8be52166f8eb0836f491d8eec34ba353a
sakshiaks12345 <sakshiaks12345@gmail.com>
2026-09-16 20:02:56 +0530

    deployement

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app =express()
+const port = process.env.PORT ||5000
+
+app.get('/', (req,res)=>{
+    return res.status(200).json({message: "hello ankush jj"})
+})
+app.get('/health', (req,res)=>{
+    return res.status(200).json({message: "all is good"})
+})
+
+app.listen(port, () =>{
+    console.log('server is started at : ', port);
+})
